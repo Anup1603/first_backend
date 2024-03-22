98f33efd3f24fae5556a216c9f31e03e779cf438
Anup1603 <anupshaw1603@gmail.com>
2024-03-22 11:49:55 +0530

    github data

--- index.js
+++ index.js
@@ -20,3 +20,42 @@ app.get('/login', (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log(`Example app listen in port ${port}`);
 })
+
+const githubData = {
+    "login": "anupshaw",
+    "id": 110482287,
+    "node_id": "U_kgDOBpXTbw",
+    "avatar_url": "https://avatars.githubusercontent.com/u/110482287?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/anupshaw",
+    "html_url": "https://github.com/anupshaw",
+    "followers_url": "https://api.github.com/users/anupshaw/followers",
+    "following_url": "https://api.github.com/users/anupshaw/following{/other_user}",
+    "gists_url": "https://api.github.com/users/anupshaw/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/anupshaw/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/anupshaw/subscriptions",
+    "organizations_url": "https://api.github.com/users/anupshaw/orgs",
+    "repos_url": "https://api.github.com/users/anupshaw/repos",
+    "events_url": "https://api.github.com/users/anupshaw/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/anupshaw/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 11,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2022-08-02T15:49:24Z",
+    "updated_at": "2022-12-23T03:20:38Z"
+}
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
+})
